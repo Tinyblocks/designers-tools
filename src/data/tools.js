b0969365250ad0fbc35355661d45709f3326718e
Colin Moinard <hello@tinyblocks.co>
2025-11-04 16:41:21 +0900

    Update tools data and add UI component showcase icon

--- src/data/tools.js
+++ src/data/tools.js
@@ -9,6 +9,14 @@ export const tools = [
   },
   {
     id: 2,
+    icon: '/assets/icons/ui-component-showcase-icon.svg',
+    title: 'UI Component Showcase',
+    description: 'Compare popular development libraries side by side. See how Material-UI, Ant Design, Tailwind CSS, Bootstrap, and more implement the same components.',
+    link: 'http://components.pixelkit.net/',
+    buttonText: 'View Demo'
+  },
+  {
+    id: 3,
     icon: '',
     title: 'More Tools Coming Soon',
     description: 'Working on new design tools and calculators to help designers streamline their workflow. Stay tuned for updates.',
